ddeb188db7b3a897539344d09349e66491fec3cd
Ritwick Das <ritwick35rd@live.com>
2023-07-02 02:12:29 +0530

    - added a separate auth controller
    - refactored the req, res handlers in the new auth controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,47 @@
+const User = require('../models/User.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const secret = 'samplesecret';
+const registerController = async (req, res) => {
+    const { name, email, password } = req.body; // getting name, email, password
+
+    if (!name || !email || !password) return void res.status(500).send({ status: 'failure', message: "required fields missing to create an user" }) //name, email or password missing
+
+    try {
+        const user = await User.create({
+            name, email, password
+        })
+        console.log(user instanceof User)
+        const token = jwt.sign({ id: user._id }, secret, { expiresIn: 3600 }) // creating a new token
+
+        res.status(200).send({ status: 'success', message: 'user created', token })
+    } catch (e) {
+        return void res.status(500).send({ status: 'failure', message: "There was a problem in registering the user" })
+    }
+}
+
+const loginController = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) return void res.status(500).send({ status: 'failure', message: "required fields missing to log in an user" })
+
+    const user = await User.findOne({ "semail": email })
+    if (user) // if user found, compare 
+    {
+        bcrypt.compare(password, user.password, function (err, isMatch) {
+            if (err) return void res.status(500).send({ status: 'failure', message: "there was a problem in logging in the user" });
+
+            if (isMatch) {
+                const token = jwt.sign({ id: user._id }, secret, {
+                    expiresIn: 3600 // expires in 24 hours
+                });
+                return void res.status(200).send({ status: 'success', message: 'logged in', token, user })
+            }
+            else return void res.status(401).send({ status: 'failure', message: "wrong password" })
+        });
+    }
+
+    else return void res.status(404).send({ status: 'failure', message: 'No user found.' });
+}
+
+module.exports = { registerController, loginController }
